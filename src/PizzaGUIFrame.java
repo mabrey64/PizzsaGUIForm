195adad1198fd8bc52b30d5492fc3eba49cb4dae
mabreydn <mabreydn@mail.uc.edu>
2025-03-02 16:02:15 -0500

    Adding Code
    Added in more code for the main panels, as well as adding the code for the calculation.

--- src/PizzaGUIFrame.java
+++ src/PizzaGUIFrame.java
@@ -5,9 +5,12 @@ public class PizzaGUIFrame extends JFrame {
     JPanel crustPanel;
     JPanel sizePanel;
     JPanel toppingsPanel;
+    JPanel buttonPanel;
+    JPanel totalPanel;
 
     JLabel titleLabel;
-    JLabel buttonLabel;
+
+    JTextArea totalArea;
 
     JRadioButton thinCrustButton;
     JRadioButton regularCrustButton;
@@ -26,6 +29,12 @@ public class PizzaGUIFrame extends JFrame {
     JButton clearButton;
     JButton exitButton;
 
+    public int crustPrice;
+    public int sizePrice;
+    public int toppingsPrice;
+    public int total;
+    public double tax = .07;
+
     public PizzaGUIFrame() {
         mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
@@ -33,6 +42,7 @@ public class PizzaGUIFrame extends JFrame {
         createCrustPanel();
         createSizePanel();
         createToppingsPanel();
+        createButtonPanel();
 
         setTitle("Pizza Order");
         setSize(400, 400);
@@ -102,5 +112,95 @@ public class PizzaGUIFrame extends JFrame {
         mainPanel.add(toppingsPanel);
     }
 
-    public 
+    public void createButtonPanel() {
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+
+        orderButton = new JButton("Order");
+        clearButton = new JButton("Clear");
+        exitButton = new JButton("Exit");
+
+        orderButton.addActionListener(e -> calculateTotal());
+        clearButton.addActionListener(e -> {
+            thinCrustButton.setSelected(false);
+            regularCrustButton.setSelected(false);
+            deepDishCrustButton.setSelected(false);
+            pizzaSizeBox.setSelectedIndex(0);
+            pepperoniBox.setSelected(false);
+            sausageBox.setSelected(false);
+            baconBox.setSelected(false);
+            taterTotsBox.setSelected(false);
+            pineappleBox.setSelected(false);
+            chickenBox.setSelected(false);
+        });
+        exitButton.addActionListener(e -> System.exit(0));
+
+        buttonPanel.add(orderButton);
+        buttonPanel.add(clearButton);
+        buttonPanel.add(exitButton);
+
+        mainPanel.add(buttonPanel);
+    }
+
+    public int calculateCrustPrice() {
+        if (thinCrustButton.isSelected()) {
+            crustPrice = 5;
+        } else if (regularCrustButton.isSelected()) {
+            crustPrice = 7;
+        } else if (deepDishCrustButton.isSelected()) {
+            crustPrice = 9;
+        }
+
+        return crustPrice;
+    }
+
+    public int calculateSizePrice() {
+        if (pizzaSizeBox.getSelectedIndex() == 0) {
+            sizePrice = 8;
+        } else if (pizzaSizeBox.getSelectedIndex() == 1) {
+            sizePrice = 12;
+        } else if (pizzaSizeBox.getSelectedIndex() == 2) {
+            sizePrice = 16;
+        } else if (pizzaSizeBox.getSelectedIndex() == 3) {
+            sizePrice = 20;
+        }
+
+        return sizePrice;
+    }
+
+    public int calculateToppingsPrice() {
+        toppingsPrice = 0;
+
+        if (pepperoniBox.isSelected()) {
+            toppingsPrice += 1;
+        }
+        if (sausageBox.isSelected()) {
+            toppingsPrice += 1;
+        }
+        if (baconBox.isSelected()) {
+            toppingsPrice += 1;
+        }
+        if (taterTotsBox.isSelected()) {
+            toppingsPrice += 1;
+        }
+        if (pineappleBox.isSelected()) {
+            toppingsPrice += 1;
+        }
+        if (chickenBox.isSelected()) {
+            toppingsPrice += 1;
+        }
+
+        return toppingsPrice;
+    }
+
+    // Calculate the price of the pizza
+    public int calculateTotal()
+    {
+
+    }
+
+    public returnTotal()
+    {
+        
+    }
 }
